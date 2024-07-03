fd6b837523c755767c835b243dbbebfa5f903450
Vidhyanand CS <vithushaji@gmail.com>
2024-07-03 15:37:03 +0530

    derivation & regix added. listed out basic to-do

--- protocol/AlgorandProtocol.ts
+++ protocol/AlgorandProtocol.ts
@@ -2,7 +2,8 @@ import { Domain, MainProtocolSymbols } from '@airgap/coinlib-core'
 import BigNumber from '@airgap/coinlib-core/dependencies/src/bignumber.js-9.0.0/bignumber'
 import { BalanceError, ConditionViolationError } from '@airgap/coinlib-core/errors'
 import { encodeDerivative } from '@airgap/crypto'
-import {encodeAddress} from "algosdk"
+import algosdk from "algosdk"
+import {algodClient, indexerClient} from "../config/algosdk"
 import {
   Address,
   AirGapProtocol,
@@ -52,13 +53,17 @@ export class AlgorandProtocolImpl implements AlgorandProtocol {
     ALGO: {
       symbol: { value: 'ALGO', market: 'algo' },
       decimals: 6
+    },
+    microAlgo: {
+      symbol:{ value: 'microAlgo'},
+      decimals: 0
     }
   }
 
   private readonly feeDefaults: FeeDefaults<AlgorandUnits> = {
-    low: newAmount(0.0001, 'ALGO'),
-    medium: newAmount(0.0001, 'ALGO'),
-    high: newAmount(0.0001, 'ALGO')
+    low: newAmount(1000, 'microAlgo'),
+    medium: newAmount(1000, 'microAlgo'),
+    high: newAmount(1000, 'microAlgo')
   }
 
   private readonly metadata: ProtocolMetadata<AlgorandUnits> = {
@@ -72,8 +77,13 @@ export class AlgorandProtocolImpl implements AlgorandProtocol {
       defaults: this.feeDefaults
     },
 
-    //derivation to-do
-  
+    account: {
+      standardDerivationPath: `m'/44'/283'/0'/0/0`,
+      address: {
+        isCaseSensitive: true,
+        regex: '^[A-Z2-7]{57}[AEIMQUY4]$'
+      }
+    }
   }
 
   public async getMetadata(): Promise<ProtocolMetadata<AlgorandUnits>> {
@@ -81,18 +91,24 @@ export class AlgorandProtocolImpl implements AlgorandProtocol {
   }
 
   private readonly cryptoConfiguration: AlgorandCryptoConfiguration = {
-    algorithm: 'secp256k1'
+    algorithm: 'ed25519'
   }
 
   public async getCryptoConfiguration(): Promise<AlgorandCryptoConfiguration> {
     return this.cryptoConfiguration
   }
 
+
 //offline
 
-  //getKeyPairFromDerivative
+  //derive key-value from derivative 
 
   //get address from public key
+    //convert publickey to uint8array
+    //derive address using sdk
+    private async getAddressFromPublicKey(publicKey: PublicKey): Promise<string>{
+      const addr = algosdk.encodeAddress(publicKey);
+    }
 
   //sign transaction with secret key
 
@@ -100,19 +116,52 @@ export class AlgorandProtocolImpl implements AlgorandProtocol {
 
 //online
 
-  //get address from public key
-
   //Get details of list of transaction
 
   //Get balance of public key
+  public async getBalanceOfPublicKey(publicKey: PublicKey, configuration?: undefined): Promise<Balance<'ALGO'>> {
+
+    //generate address
+    //get balance from address
+    
+  }
 
   //Get maximum amount that can be transacted
+  public async getTransactionMaxAmountWithPublicKey(publicKey: PublicKey, to: string[], configuration?: TransactionFullConfiguration<'ALGO'> | undefined): Promise<Amount<'ALGO'>> {
+    const account_info = await algodClient.accountInformation(address).do()
+    const availableBalance = algosdk.microalgosToAlgos(
+      (account_info["amount"] - account_info["min-balance"])
+    )
+  }
 
-  //Get transaction fee from public key
+  
+  public async  prepareTransactionWithPublicKey(publicKey: PublicKey, details: TransactionDetails<AlgorandUnits>[], configuration?: TransactionFullConfiguration<AlgorandUnits> | undefined): Promise<AlgorandUnsignedTransaction> {
+    const AlgorandUnsignedTransaction = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from,
+      to,
+      amount,
+      closeRemainderTo,
+      note,
+      suggestedParams
+    })
+  }
 
-  //get details from unsigned transaction
 
   //get details from signed transaction
+  public async getDetailsFromUnsignedTransaction(transaction: AlgorandnUnsignedTransaction): Promise<AirGapTransaction<AlgoUnits>[]>{
+    return [
+      {
+        from,
+        to,
+        amount,
+        fee,
+        note,
+        network
+      }
+    ]
+  }
+
+  //get details from unsigned transaction
 
   //broadcast transaction
   
