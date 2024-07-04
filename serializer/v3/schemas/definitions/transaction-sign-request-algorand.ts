cad47f5404914753248ddc9ccedbe68bc6865055
Vidhyanand CS <vithushaji@gmail.com>
2024-07-04 16:07:38 +0530

    added serializer schema definition

---
+++ serializer/v3/schemas/definitions/transaction-sign-request-algorand.ts
@@ -0,0 +1,5 @@
+import { TransactionSignRequest } from '@airgap/serializer'
+
+import { AlgorandUnsignedTransaction } from '../../../../types/transaction'
+//just copied bitcoin implemetation. change omit if we have to provide support for ASAs. To-do more research
+export interface MinaTransactionSignRequest extends TransactionSignRequest<Omit<AlgorandUnsignedTransaction, 'type'>> {}
