b5d9d6764c8a6f940745bd011d3fca2eeed4ae44
Vidhyanand CS <vithushaji@gmail.com>
2024-07-08 17:03:41 +0530

    correction in interface name

--- serializer/v3/schemas/definitions/transaction-sign-request-algorand.ts
+++ serializer/v3/schemas/definitions/transaction-sign-request-algorand.ts
@@ -2,4 +2,4 @@ import { TransactionSignRequest } from '@airgap/serializer'
 
 import { AlgorandUnsignedTransaction } from '../../../../types/transaction'
 //just copied bitcoin implemetation. change omit if we have to provide support for ASAs. To-do more research
-export interface MinaTransactionSignRequest extends TransactionSignRequest<Omit<AlgorandUnsignedTransaction, 'type'>> {}
+export interface AlgorandTransactionSignRequest extends TransactionSignRequest<Omit<AlgorandUnsignedTransaction, 'type'>> {}
